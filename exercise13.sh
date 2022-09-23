73fb1ce562bb5b4f811e2e80be2fef3afbe9e6e7
Caden Hong <caden.p.hong@gmail.com>
2022-09-23 14:18:33 -0400

    Editing exercise 13

--- exercise13.sh
+++ exercise13.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-Write a script to print chess board
+#Write a script to print chess board
 
-Prerequisites:
-Knowledge about printing colors using echo
-Use of nested loops.
+#Prerequisites:
+#Knowledge about printing colors using echo
+#Use of nested loops.
 
-Objective:
-Print colors using echo command.
+#Objective:
+#Print colors using echo command.
 
-Requirements:
-To print a black box echo -e -n “\\\\e[40m” ” “
-To print a white box echo -e -n “\\\\e[47m” ” “
-Call the commands in a loop.
-After 8 columns make to normal color.
-To make it normal echo -e -n “\\\\e[0m” ” “
+#Requirements:
+#To print a black box echo -e -n “\\\\e[40m” ” “
+#To print a white box echo -e -n “\\\\e[47m” ” “
+#Call the commands in a loop.
+#After 8 columns make to normal color.
+#To make it normal echo -e -n “\\\\e[0m” ” “
 
 for (( i = 1 ; i <= 8 ; i++ )); do
 
-    for (( j = 1 ; j <= 8 ; j+2 )); do
-        echo -e -n “\\\\e[40m” ” “
-    done
+    if [[ $i%2 -eq 0 ]]; then
+	for (( j = 1 ; j <= 8 ; j+=2 )); do
+            echo -e -n "\e[1;40m   \e[0m"
+        done
 
-    for (( k = 2 ; k <= 8 ; k+2 )); do
-        echo -e -n “\\\\e[47m” ” “
-    done
+        for (( k = 2 ; k <= 8 ; k+=2 )); do
+            echo -e -n "\e[1;47m   \e[0m"
+        done
+
+    else
+        for (( m = 1 ; m <= 8 ; m+=2 )); do
+            echo -e -n "\e[1;47m   \e[0m"
+        done
+
+	for (( n = 2 ; n <= 8 ; n+=2 )); do
+            echo -e -n "\e[1;40m   \e[0m"
+	done
+    fi
+
+    echo
+
 done
