4e33a26c67540dcb49360916773eaa17f3d153de
Caden Hong <caden.p.hong@gmail.com>
2022-09-25 21:59:02 -0400

    initial commit of 15

---
+++ exercise15.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+Write a script to print the following system information:
+- Currently logged users
+- Your shell directory
+- Home directory
+- OS name & version
+- Current working directory
+- Number of users logged in
+- Show all available shells in your system
+- Hard disk information
+- CPU information
+- Memory information
+- File system information
+- Currently running process
+
+Prerequisites:
+- Knowledge about user commands w, who, whoami
+- Bash environmental variables
+- /proc file-system
+- Other system info commands like df, du, uname, ps
+
+Objective:
+- To learn system information commands
+
+Requirements:
+- Provide a menu for user about what information he wants to check
+- Using switch case display output for selected option
+
