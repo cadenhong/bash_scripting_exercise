f664878b3d4ecbf11a390aa4088e86d1da95174b
Caden Hong <caden.p.hong@gmail.com>
2022-09-28 13:54:04 -0400

    initial commit of e17

---
+++ exercise17.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Write a script to rename a file/directory replaced by lower/upper case letters
+
+# Prerequisites:
+# - Knowledge about mv and tr commands.
+# - To check a file type in script.
+# - WARNING: Dont try this in your home/ or assignment/ directory.
+#   Please create a seperate directory to test this script.
+
+# Objective:
+# - To learn filter/translate commands
+# - Identifying file types in script
+
+# Requirements:
+# - Rename all files from current directory to lowercase letters.
+# - Rename all directories from current directories to uppercase.
+# - Digits and other symbols should remain same.
