761e0bec8c74685a30380ec52f80e4f01f368713
Caden Hong <caden.p.hong@gmail.com>
2022-10-03 12:11:33 -0400

    longest and shortest usernames in the system

--- exercise23.sh
+++ exercise23.sh
@@ -14,6 +14,23 @@
 # - Fetch user-names from the first field in /etc/passwd file.
 # - Print longest and shortest name.
 
-usernames=( $(sudo cat /etc/passwd | awk -F: '{print $1}') )
-declare -p usernames
+usernames=( $(cat /etc/passwd | awk -F: '{print $1}') )
+total=${#usernames}
 
+longest=${usernames[0]}
+shortest=${usernames[0]}
+
+for i in ${!usernames[@]}; do
+  current=${usernames[i]}
+  currLen=${#current}
+
+  if [ "$currLen" -gt "${#longest}" ]; then
+    longest=$current
+  elif [ "$currLen" -lt "${#shortest}" ]; then
+    shortest=$current
+  fi
+
+done
+
+echo "Longest username: $longest"
+echo "Shortest username: $shortest"
