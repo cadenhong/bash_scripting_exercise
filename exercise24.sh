e4e53d4177c3c0f83e2de5ca52b4301141e7ede8
Caden Hong <caden.p.hong@gmail.com>
2022-10-03 20:43:56 -0400

    find and delete all .swp files

--- exercise24.sh
+++ exercise24.sh
@@ -14,17 +14,17 @@
 # - If no arguments passed delete from entire ~/ directory
 # - If no file present show a message.
 
+if [ $# -eq 0 ]; then
+    location=~/
+else
+    location=$1
+fi
 
 tempfiles=$(find $location -name "*.swp" -type f | wc -l)
 
 if [ $tempfiles -eq 0 ]; then
     echo "No .swp files are present!"
 else
-    if [ $# -eq 0 ]; then
-        location="~/"
-    else
-        location=$1
-    fi
     find $location -name "*.swp" -type f -delete
-    echo "Total of $tempfiles were deleted from $location"
+    echo "Total of $tempfiles .swp file(s) deleted from $location"
 fi
