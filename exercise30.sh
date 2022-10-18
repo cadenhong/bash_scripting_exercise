bcce4730a6a799704c869ee4d5082d20addf60e8
Caden Hong <caden.p.hong@gmail.com>
2022-10-18 00:21:19 -0400

    change permissions

---
+++ exercise30.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Write a script to locks down file permissions for a particular directory
+
+# Prerequisites:
+# Must know working of chmod command.
+
+# Objective:
+# Learn about file permissions.
+
+# Requirements:
+# Remove all permissions for groups and others.
+# Provide directory name through command-line.
+# After running script all files in the given directory, Only should have all the permissions.
+# But remember dont add any permission to user only change to others and groups.
+
+dir_name=$@
+
+cd $dir_name
+
+for f in *:
+    chmod og-rwx $f
+done
