4c99ee689ee96be7a66bdb8a8e887a23ef335849
Caden Hong <caden.p.hong@gmail.com>
2022-10-19 13:16:16 -0400

    instruction for 31

---
+++ exercise31.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Display the names of any file-system which have less than 10% free space available
+
+# Prerequisites:
+# Must know commands df, tr and cut
+# Use of arrays and loops.
+
+# Objective:
+# Learn more about mounting, file-systems and device files.
+
+# Requirements:
+# When you run the script show all file-system present in system.
+# Then print file-systems that have only 10% memory remaining.
+
