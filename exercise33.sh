c48339a1748ca0932f62dcbccd9f983086c2f71d
Caden Hong <caden.p.hong@gmail.com>
2022-10-19 13:42:20 -0400

    33

---
+++ exercise33.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# For each directory in the $PATH, display the number of executable files in that directory
+
+# Prerequisites:
+# Must know bash environmental variables.
+# Working of tr command.
+# Loops and arrays.
+# Checking permission of files in script.
+
+# Objective:
+# Learn significance of PATH variable.
+
+# Requirements:
+# Fetch each directories from PATH variable.
+# Use -x option if if condition to check executable permission.
+# Print directory and number of executable files one-by-one.
+# Print the total number of executable files at last.
+# Count only files have executable permission.
+# Verify path is present every-time.
+
