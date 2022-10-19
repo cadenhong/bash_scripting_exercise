075e333791e379e69502be453d8a864c86f0d89c
Caden Hong <caden.p.hong@gmail.com>
2022-10-19 13:19:55 -0400

    display filesystems lt 10% free space

--- exercise31.sh
+++ exercise31.sh
@@ -13,3 +13,11 @@
 # When you run the script show all file-system present in system.
 # Then print file-systems that have only 10% memory remaining.
 
+name=( $(df -h | tail -n +2 | awk '{print $1}') ) # array of filesystem names
+usage=( $(df -h | tail -n +2 | awk '{print $5}' | sed 's/\%//') ) # array of usag>
+
+for (( i=0; i<${#usage[@]}; i++ )); do # for loop to iterate thru usage array
+  if [ ${usage[$i]} -ge 90 ]; then # if statement to check if usage is > 90
+    echo ${name[$i]} # echo the element with same index in name array
+  fi
+done
